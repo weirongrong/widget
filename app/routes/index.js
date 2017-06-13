d7b8eed39929bf229600db54741049c298c49a8f
weirong <weirong@vicancy.com>
2017-06-13 14:22:55 +0200

    testing post 9

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
  model(){
-     return Ember.$.ajax('https://dashboard-staging.hrofficelabs.com');
+    //  return Ember.$.ajax('https://dashboard-staging.hrofficelabs.com');
     //  return fetch('https://dashboard-staging.hrofficelabs.com')
     //     .then (function(response){
     //         return response;
