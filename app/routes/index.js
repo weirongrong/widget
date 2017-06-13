9998f3f5a829efd0da91022e4cdfe3947ccfa038
weirong <weirong@vicancy.com>
2017-06-13 14:26:40 +0200

    testing post 10

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,11 @@ import Ember from 'ember';
 
 
 export default Ember.Route.extend({
+    fastboot: Ember.inject.service(),
  model(){
+    
+    let headers = this.get('https://obscure-brushlands-65803.herokuapp.com/');
+    let xRequestHeader = headers.get('X-Request');
     //  return Ember.$.ajax('https://dashboard-staging.hrofficelabs.com');
     //  return fetch('https://dashboard-staging.hrofficelabs.com')
     //     .then (function(response){
