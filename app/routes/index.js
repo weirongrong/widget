90b9a7691ec0e3c439b016f266e5a3980cc3eda7
weirong <weirong@vicancy.com>
2017-06-13 15:13:07 +0200

    testing post 14

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 
 export default Ember.Route.extend({
-    fastboot: Ember.inject.service(),
+    // fastboot: Ember.inject.service(),
  model(){
     
     // let headers = this.get('https://dashboard-staging.hrofficelabs.com/');
